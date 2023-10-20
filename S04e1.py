4c8b4c0d0d1241aad23c818ee42d6e160d220f53
Farima Torabzadeh <82698117+farima-trb@users.noreply.github.com>
2023-10-20 20:09:26 +0330

    Add files via upload

---
+++ S04e1.py
@@ -0,0 +1,14 @@
+import re
+regex = r'\b[A-Za-z0-9]+@[A-Za-z]+\.[A-Z|a-z]{2,}\b'
+
+if __name__ == '__main__':
+    temp=input() 
+    
+    if (re.fullmatch(regex, temp)!=None):
+        print("OK")
+    
+    else:
+        while(re.fullmatch(regex, temp)==None):
+            print("WRONG")
+            temp=input() 
+            continue
