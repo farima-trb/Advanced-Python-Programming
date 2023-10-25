f83d4b9bb76f2ff85129182c069d94ac7f880f3c
Farima Torabzadeh <82698117+farima-trb@users.noreply.github.com>
2023-10-25 14:23:59 +0330

    Update Final_Project.py

--- Final_Project.py
+++ Final_Project.py
@@ -3,8 +3,12 @@ from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
 from bs4 import BeautifulSoup
 import pandas as pd
-from mysql.connector import connect,Error
 import mysql.connector
+from sqlalchemy import create_engine
+from sklearn.model_selection import train_test_splits
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler
+import numpy as np
 
 session = requests.Session()
 retry = Retry(connect=3, backoff_factor=0.5)
@@ -63,7 +67,66 @@ current_year=2023
 df2["Year"]=list(map(lambda y: current_year-y,list(df2["Year"])))
 
 df2["Transmission"]=list(map(lambda y: y.replace('FWD','Automatic Transmission').replace('RWD','Automatic Transmission'),list(df2["Transmission"])))
+# Lable the categorical data 0 for Auto , 1 for Manual
+df2["Transmission"]= LabelEncoder().fit_transform(df2["Transmission"])
 
 df2.to_csv('cleaned_data.csv', index=False)
 
+# Adding data to database table
+conn=mysql.connector.connect(host="localhost", username="root",
+                             password="SQLtest6384",database="Cars")
 
+cursor=conn.cursor()
+
+sql ="""CREATE TABLE CarInfo_test( Name VARCHAR(10), 'Price' VARCHAR(10),
+        'Mileage' VARCHAR(10), 'Year' INT, 'Transmission' VARCHAR(25))"""
+        
+engine = create_engine("mysql+pymysql://{user}:{pw}@{host}/{db}"
+				.format(host="localhost", db="Cars", user="root", pw="SQLtest6384"))
+
+df2.to_sql('CarInfo', engine, index=False)
+
+frame = pd.read_sql("select * from Cars.CarInfo", conn)
+pd.set_option('display.expand_frame_repr', False)
+
+conn.commit()
+conn.close() 
+    
+    
+# Prediction
+
+# correlation check
+corr = frame.corr()
+# print(corr)
+
+# positive correlation --> Mileage & Year
+# negative correlation --> price & Mileage , Price & Year, Price & Transmission
+
+frame = frame.drop(['Name'], axis=1)
+
+# Create X and Y variables
+# Mileage, Year, Transmission are X  &  Price is Y
+
+X = frame.drop('Price', axis=1)
+y = np.log(frame['Price'])
+
+# Train and Test Splitting
+X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.3, random_state=42)
+
+# Normalisation
+norm = MinMaxScaler().fit(X_train)
+X_train = norm.transform(X_train)
+X_test = norm.transform(X_test)
+
+# Implementing Linear Regression
+model= LinearRegression()
+model.fit(X, y)
+y_pred = model.predict(X)
+# print(f"predicted response:\n{y_pred}")
+
+print(f"coefficient of determination: {model.score(X, y)}")
+print(f"intercept: {model.intercept_}")
+print(f"coefficients: {model.coef_}")
+    
+frame.insert(4, "Predicted Price", np.exp(y_pred), True)
+print(frame)
